9c446f2004be9dfd545e1dbaa87e31f0c343d1fc
Abigail <abialvarado14@unitec.edu>
2017-03-17 11:57:11 -0600

    Ultimo commit

--- main.cpp
+++ main.cpp
@@ -9,16 +9,31 @@
 using namespace std;
 
 int main(){
-	int opc;
-	int hp;
+	int opc,hp,fuerza,luck, opcTipo, defensa,nivel;
 	string name;
+
+	vector <Bender*> Benders;
 	cout << "Que desea hacer? 1.Agregar \n2.Jugar";
 	cin >>opc;
-	if (op==1){//AGREGAR
+	if (opc==1){//AGREGAR
 		cout << "Ingrese el nombre del Avatar:";
 		cin >> name;
 		cout << "Ingrese el HP del avatar:";
 		cin >> hp;
+		cout << "Ingrese la defensa del avatar:";
+		cin >> defensa;
+		cout << "Ingrese el nivel de suerte:";
+		cin >> luck;
+
+		while (luck <0||luck>10){
+			cout << "Ingrese nuevamente el nivel de suerte:";
+			cin >> luck;
+		}
+		cout << "Ingrese la fuerza del avatar:";
+		cin >> fuerza;
+		cout << "Que tipo de avatar es??:\n1.Firebender\n2.Waterbender\n3.Earhbender\n4.Airbender";
+		cin >> opcTipo;
+
 	}else{
 
 	}
