be1baf0d5c31dcaf4033ea3c1195e9bf5a30c3b8
Abigail <abialvarado14@unitec.edu>
2017-03-17 11:38:38 -0600

    Change in main

--- main.cpp
+++ main.cpp
@@ -10,12 +10,17 @@ using namespace std;
 
 int main(){
 	int opc;
+	int hp;
+	string name;
 	cout << "Que desea hacer? 1.Agregar \n2.Jugar";
 	cin >>opc;
 	if (op==1){//AGREGAR
-
+		cout << "Ingrese el nombre del Avatar:";
+		cin >> name;
+		cout << "Ingrese el HP del avatar:";
+		cin >> hp;
 	}else{
-		
+
 	}
 	return 0;
 }
