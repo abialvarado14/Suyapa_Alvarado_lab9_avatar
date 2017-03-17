2dc115bf59d24fd05725662b26376e9730dea41b
Abigail <abialvarado14@unitec.edu>
2017-03-17 11:35:27 -0600

    Main

--- main.cpp
+++ main.cpp
@@ -3,8 +3,15 @@
 #include "earthbender.h"
 #include "firebender.h"
 #include "waterbenders.h"
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
 
 int main(){
-
+	int opc;
+	cout << "Que desea hacer? 1.Agregar \n2.Jugar";
+	cin >>opc;
+	
 	return 0;
 }
